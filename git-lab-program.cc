ef6e88b435e82822116ebbb3d5869b5fdf472445
Jacob Murphy <jm443918@ohio.edu>
2019-05-17 03:26:35 +0100

    Add Hello Git!

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jacob Murphy
+ *        Date: 5/16/2017
+ * Description: Homework 1 Part 15
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    << "Hello Git!!" << endl;
+    return 0;
+}// main
