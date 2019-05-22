6ede4abe30f5495a7eb5d4679b3ca71f027f1a05
Jacob Murphy <jm443918@ohio.edu>
2019-05-22 23:31:05 +0100

    Added Description

---
+++ gas_consumption.cc
@@ -0,0 +1,21 @@
+/*
+ *        File: gas_consumption.cc
+ *      Author: Jacob Murphy
+ *        Date: 05/22/2019
+ * Description: Quiries user for miles driven in town and highway. Given gas comsumption rate for both, returns gas cunsumption and miles driven
+ *              for both regions and total, along with average consumption rate
+ *
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+   
+   
+   
+   return 0;
+}// main
